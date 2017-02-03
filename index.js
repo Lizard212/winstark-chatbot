550d997c02dba5ef125480636441f8cbf016a8d5
Lizard212 <phamthangnd212@gmail.com>
2017-02-03 12:05:26 +0700

    add simple chatbot server

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
+var logger = requie('morgan');
+var http = require('http');
+var request = require('request');
+var bodyParser = require('body-parser');
 var express = require('express');
+var router = express();
+
+
 var app = express();
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var server = http.createServer(app);
+
+app.get('/', (req, res)=> {
+  res.send("Hello. This is index page !You're welcome here !")
+});
+
+app.get('/webhook', function(req, res) {
+  if(req.query['hub.verify_token'] === 'winter_is_coming'){
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+});
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(__dirname + '/public'));
-
-// views is directory for all template file
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response) {
-  response.render('pages/index');
+server.listen(app.get('port'), function() {
+  console.log('Chat bot server listening at ', app,get('port'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
 
