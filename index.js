0b2f889dbdab4320583321afa91ace60a1ca4930
Lizard212 <phamthangnd212@gmail.com>
2017-02-03 16:52:41 +0700

    add config/default.config

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+'use strict';
+
+const
+config = require('config');
+
 var logger = require('morgan');
 var http = require('http');
 var request = require('request');
@@ -12,6 +17,28 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: false
 }));
+
+const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ? 
+  process.env.MESSENGER_APP_SECRET :
+config.get('appSecret');
+
+const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
+  (process.env.MESSENGER_VALIDATION_TOKEN) :
+config.get('validationToken');
+
+const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
+  (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
+config.get('pageAccessToken');
+
+const SERVER_URL = (process.env.SERVER_URL) ?
+  (process.env.SERVER_URL) :
+  config.get('serverURL');
+
+if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
+  console.error("Missing config values");
+  process.exit(1);
+}
+
 
 var server = http.createServer(app);
 
